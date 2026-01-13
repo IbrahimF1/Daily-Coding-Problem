3eef395899d064cfb8e8591506cc58d116c74e57
IbrahimF1 <ifaruquee1@gmail.com>
2026-01-12 23:58:20 -0500

    Grind 75, Week 1, Insert Interval

---
+++ grind75/Week1/insert-interval.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/insert-interval
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        
